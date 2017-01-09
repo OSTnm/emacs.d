7ccfac9016a21158c34e18159ccaafa634faead9
lixuan <lixuan_1989@163.com>
2017-01-09 22:26:13 +0800

    add plugin - bug hunter
    Signed-off-by: lixuan <lixuan_1989@163.com>

---
+++ elpa/bug-hunter-1.3.1/bug-hunter-autoloads.el
@@ -0,0 +1,15 @@
+;;; bug-hunter-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("bug-hunter.el") (22643 40281 795582 703000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bug-hunter-autoloads.el ends here
