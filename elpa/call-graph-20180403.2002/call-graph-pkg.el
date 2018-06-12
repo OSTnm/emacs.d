be5886cd7c9a1f9e946cd40497a738eade141028
xuali2 <xuali2@cisco.com>
2018-06-13 00:07:33 +0800

    Update plugin.
    Signed-off-by: Xuan Li <xuali2@cisco.com>

--- elpa/call-graph-20180403.2002/call-graph-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "call-graph" "20180403.2002" "Library to generate call graph for c/c++ functions" '((emacs "25.1") (cl-lib "0.6.1") (hierarchy "0.7.0") (tree-mode "1.0.0") (ivy "0.10.0")) :commit "31bfac00f33b8494fcf6c18a210dd7ed4b81a21d" :keywords '("programming" "convenience") :url "https://github.com/beacoder/call-graph")
---
+++ elpa/call-graph-20180509.635/call-graph-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "call-graph" "20180509.635" "Library to generate call graph for c/c++ functions" '((emacs "25.1") (cl-lib "0.6.1") (hierarchy "0.7.0") (tree-mode "1.0.0") (ivy "0.10.0")) :commit "1ba83f20e56cfe77f6165df4ffde1d152647ec3b" :keywords '("programming" "convenience") :url "https://github.com/beacoder/call-graph")
