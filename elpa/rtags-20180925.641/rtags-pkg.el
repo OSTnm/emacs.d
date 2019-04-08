e2ff8c8e0513cb59b4dc902113725045ac33f52d
xuali2 <xuali2@cisco.com>
2019-04-08 14:26:25 +0800

    Update plugin.
    Signed-off-by: Xuan Li <xuali2@cisco.com>

--- elpa/rtags-20180925.641/rtags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rtags" "20180925.641" "A front-end for rtags" 'nil :commit "d388a00f54c67b09af93ca0cc98c35433ebe7910" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
